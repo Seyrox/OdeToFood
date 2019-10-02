acb2bcbaf96df34252f3634237269d95a0b7bee3
Seyrox <takeandrei5@gmail.com>
2019-10-02 15:03:47 +0300

    Finished Database Connection, added initialMigration and some data. Changes to appsettings aswell

---
+++ OdeToFood.Data/SqlRestaurantData.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+using OdeToFood.Core;
+
+namespace OdeToFood.Data
+{
+    public class SqlRestaurantData : IRestaurantData
+    {
+        public Restaurant AddRestaurant(Restaurant newRestaurant)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public int Commit()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Restaurant DeleteRestaurant(int restaurantID)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Restaurant GetById(int id)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public IEnumerable<Restaurant> GetRestaurantsByName(string restaurantName)
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public Restaurant Update(Restaurant updateRestaurant)
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
